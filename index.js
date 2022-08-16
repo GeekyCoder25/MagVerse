b96699345f6b5cbc16b62e14bad801015b74865b
Geeky Coder <toyibe25@gmail.com>
2022-08-16 08:22:54 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function pressMenu() {
     let x = document.getElementById('links');
-    let y = document.getElementById('searchimg');
 
 if (x.style.display === "block") {
     x.style.display = "none";
